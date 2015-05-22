78d49923d32e2da7f6e607c9c9aafc1b7c7fae87
Jan  Matas <jan.matas14@imperial.ac.uk>
2015-05-22 14:36:31 +0100

    First instruction (and) implemented

--- src/emulate.c
+++ src/emulate.c
@@ -6,6 +6,14 @@
 
 #define REGISTERS_COUNT 17
 #define MEMORY_SIZE 65536
+#define CPSR 16
+#define N_POS 31
+#define Z_POS 30
+#define C_POS 29
+#define V_POS 28
+#define S_POS 20
+
+
 
 struct State state;
 struct State {
@@ -22,14 +30,22 @@ unsigned int maskInt(unsigned int data, int upper, int lower);
 unsigned int instructionType(unsigned int instruction);
 int checkCond(unsigned int instruction);
 void initialize(void);
+void routeInstruction(unsigned int instruction);
+void dproc(unsigned int instruction);
+int dproc_and(unsigned int instruction, int operand2);
 
 int main(int argc, char **argv) {
 
     initialize();
     unsigned int testInstruction = 10 << 28;
-    state.registers[16] = 0xf << 28;
+    state.registers[CPSR] = 0xf << 28;
     unsigned int data = 0x0000000f;
     unsigned int result = maskInt(data, 5, 2);
+    
+    unsigned int instruction = 0xffffffff;
+    
+    routeInstruction(instruction);
+    
     printf("CPSR : %u \n", result);
 
     checkCond(testInstruction);
@@ -65,25 +81,34 @@ int checkCond(unsigned int instruction) {
     return 0;
 }
 
+void setFlag(int val, int pos) {
+	if (val == 0) {
+		val = 1 << pos;
+		val = ~val;
+		state.registers[CPSR] = state.registers[CPSR] & val;
+	} else {
+		val = 1 << pos;
+		state.registers[CPSR] = state.registers[CPSR] | val;
+	}
+}
+
+//TODO fix those
 unsigned int getV(void){
-    unsigned int temp = state.registers[16] << 3;
-    return temp >> 31;
+    return maskInt(state.registers[CPSR], V_POS, V_POS);
 }
 
 unsigned int getC(void){
-    unsigned int temp = state.registers[16] << 2;
-    return temp >> 31;
+    return maskInt(state.registers[CPSR], C_POS, C_POS);
 }
 
 unsigned int getZ(void) {
-    unsigned int temp = state.registers[16] << 1;
-    return temp >> 31;
+    return maskInt(state.registers[CPSR], Z_POS, Z_POS);
 }
 
 unsigned int getN (void) {
-
-    return state.registers[16] >> 31;
+    return maskInt(state.registers[CPSR], N_POS, N_POS);
 }
+
 
 unsigned int maskInt(unsigned int data, int upper, int lower) {
     assert(upper >= lower);
@@ -116,4 +141,58 @@ unsigned int instructionType(unsigned int instruction) {
         return -1;
     }
 
+}
+
+void routeInstruction(unsigned int instruction) {
+	if (!checkCond(instruction)) {
+		return;
+	}
+	int type = instructionType(instruction);
+	switch(type) {
+		case 0: 
+			dproc(instruction);
+			return;
+		case 1:
+			//multiply(instruction);
+			return;
+		case 2:
+			//sDataTrans(instruction);
+			return;
+		case 3:
+			//branch(instruction);
+			return;
+		default:			
+			return;
+	}
+}
+
+
+void dproc(unsigned int instruction) {
+	int opcode = maskInt(instruction, 24, 21);
+	int operand2 = 0; //TODO implement this
+	int result;
+	switch(opcode) {
+		case 0:
+			result = dproc_and(instruction, operand2);
+			return;
+		default:
+			return;
+	}
+	int sBit = maskInt(instruction, S_POS, S_POS);
+	if (sBit) {  
+		if (result == 0) {
+			setFlag(1, Z_POS);
+		} else {
+			setFlag(0, Z_POS);
+		}
+		setFlag(maskInt(result, 31, 31), N_POS);
+	}
+}
+
+int dproc_and(unsigned int instruction, int operand2) {
+	int rn = maskInt(instruction, 19, 16);
+	int rd = maskInt(instruction, 15, 12);
+	int result = state.registers[rn] & operand2;
+	state.registers[rd] = result;
+	return result;
 }
