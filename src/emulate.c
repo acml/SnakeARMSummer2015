bf3d386389c7dde2cc0582a7359066064842d599
Jan  Matas <jan.matas14@imperial.ac.uk>
2015-05-22 19:12:01 +0100

    almost finished adapting my functions to new struct

--- src/emulate.c
+++ src/emulate.c
@@ -87,24 +87,26 @@ ins_t insTpye(state_t *state);
 int isCondTrue(state_t *state);
 
 void initialize(const char *filename);
-void routeInstruction(uint32_t instruction);
-void dproc(uint32_t instruction);
+void routeInstruction(state_t *state);
+void dproc(state_t *state);
 
+
+void setFlag(state_t *state, uint8_t val, uint8_t pos);
 
 //DATA PROCESSING INSTRUCTIONS
-uint32_t dproc_and(uint32_t operand2, uint8_t rn, uint8_t rd);
-uint32_t dproc_eor(uint32_t operand2, uint8_t rn, uint8_t rd);
-uint32_t dproc_sub(uint32_t operand2, uint8_t rn, uint8_t rd);
-uint32_t dproc_rsb(uint32_t operand2, uint8_t rn, uint8_t rd);
-uint32_t dproc_add(uint32_t operand2, uint8_t rn, uint8_t rd);
-uint32_t dproc_tst(uint32_t operand2, uint8_t rn);
-uint32_t dproc_teq(uint32_t operand2, uint8_t rn);
-uint32_t dproc_cmp(uint32_t operand2, uint8_t rn);
-uint32_t dproc_orr(uint32_t operand2, uint8_t rn, uint8_t rd);
-uint32_t dproc_mov(uint32_t operand2, uint8_t rd);
+uint32_t dproc_and(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd);
+uint32_t dproc_eor(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd);
+uint32_t dproc_sub(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd);
+uint32_t dproc_rsb(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd);
+uint32_t dproc_add(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd);
+uint32_t dproc_tst(state_t *state, uint32_t operand2, uint8_t rn);
+uint32_t dproc_teq(state_t *state, uint32_t operand2, uint8_t rn);
+uint32_t dproc_cmp(state_t *state, uint32_t operand2, uint8_t rn);
+uint32_t dproc_orr(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd);
+uint32_t dproc_mov(state_t *state, uint32_t operand2, uint8_t rd);
 
 //DATA PROCESSING HELPERS
-uint32_t dproc_getOperand2(uint32_t instruction);
+uint32_t dproc_getOperand2(state_t *state);
 uint32_t shift(uint8_t type, uint32_t number, uint32_t offset);
 
 
@@ -137,20 +139,8 @@ int main(int argc, char **argv) {
 
 
 
-    initialize(argv[1]);
-    printf("0x%08X\n", state.memory[0]);
-    //uint32_t testInstruction = 10 << 28;
-    //state.registers[CPSR] = 0xf << 28;
-    //uint32_t data = 0x0000000f;
-    //uint32_t result = maskInt(data, 5, 2);
-
-    //uint32_t instruction = 0xffffffff;
-
-    //routeInstruction(instruction);
-
-
-
-    //checkCond(testInstruction);
+    //initialize(argv[1]);
+    printf("0x%08X\n", state->memory[0]);
     return EXIT_SUCCESS;
 }
 
@@ -171,7 +161,7 @@ uint8_t checkInput(int argc, char **argv) {
     }
     return 0;
 }
-
+/*
 void initialize(const char *filename) {
 
     state.memory = malloc(MEMORY_SIZE * sizeof(uint32_t));
@@ -182,39 +172,41 @@ void initialize(const char *filename) {
     memset(state.registers, 0, REGISTERS_COUNT);
 
 }
+*/
 
-
-void setFlag(int val, int pos) {
+void setFlag(state_t *state, int val, int pos) {
 	if (val == 0) {
 		val = 1 << pos;
 		val = ~val;
-		state.registers[CPSR] = state.registers[CPSR] & val;
+		
+		state->registers[CPSR_REG] = state->registers[CPSR_REG] & val;
 	} else {
 		val = 1 << pos;
-		state.registers[CPSR] = state.registers[CPSR] | val;
+		state->registers[CPSR_REG] = state->registers[CPSR_REG] | val;
 	}
 }
 
 
 
 // Routes instructions to a correct router
-void routeInstruction(uint32_t instruction) {
-	if (!checkCond(instruction)) {
+void routeInstruction(state_t *state) {
+  
+	if (!checkCond(state->fetched)) {
 		return;
 	}
-	int type = instructionType(instruction);
+	int type = instructionType(state->fetched);
 	switch(type) {
 		case 0:
-			dproc(instruction);
+			dproc(state);
 			return;
 		case 1:
-			multiply(instruction);
+			//multiply(instruction);
 			return;
 		case 2:
 			//sDataTrans(instruction);
 			return;
 		case 3:
-			branch(instruction);
+			//branch(instruction);
 			return;
 		default:
 			return;
@@ -222,56 +214,56 @@ void routeInstruction(uint32_t instruction) {
 }
 
 // Routes the dataProcessing instruction to a correct function
-void dproc(uint32_t instruction) {
-	int opcode = maskInt(instruction, 24, 21);
-	uint32_t operand2 = dproc_getOperand2(instruction);
+void dproc(state->fetched) {
+	int opcode = maskBits(state->fetched, 24, 21);
+	uint32_t operand2 = dproc_getOperand2(state);
 	uint32_t result;
-	uint8_t rn = maskInt(instruction, 19, 16);
-	uint8_t rd = maskInt(instruction, 15, 12);
+	uint8_t rn = maskBits(state->fetched, 19, 16);
+	uint8_t rd = maskBits(instruction, 15, 12);
 	switch(opcode) {
 		case 0:
-			result = dproc_and(operand2, rn, rd);
+			result = dproc_and(state, operand2, rn, rd);
 			return;
 		case 1:
-			result = dproc_eor(operand2, rn, rd);
+			result = dproc_eor(state, operand2, rn, rd);
 			return;
 		case 2:
-			result = dproc_sub(operand2, rn, rd);
+			result = dproc_sub(state, operand2, rn, rd);
 			return;
 		case 3:
-			result = dproc_rsb(operand2, rn, rd);
+			result = dproc_rsb(state, operand2, rn, rd);
 			return;
 		case 4:
-			result = dproc_add(operand2, rn, rd);
+			result = dproc_add(state, operand2, rn, rd);
 			return;
 		case 8:
-			result = dproc_tst(operand2, rn);
+			result = dproc_tst(state, operand2, rn);
 			return;
 		case 9:
-			result = dproc_teq(operand2, rn);
+			result = dproc_teq(state, operand2, rn);
 			return;
 		case 10:
-			result = dproc_cmp(operand2, rn);
+			result = dproc_cmp(state, operand2, rn);
 			return;
 		case 12:
-			result = dproc_orr(operand2, rn, rd);
+			result = dproc_orr(state, operand2, rn, rd);
 			return;
 		case 13:
-			result = dproc_mov(operand2, rd);
+			result = dproc_mov(state, operand2, rd);
 			return;
 
 		default:
 
 			return;
 	}
-	int sBit = maskInt(instruction, S_POS, S_POS);
+	int sBit = maskBits(state->fetched, S_POS, S_POS);
 	if (sBit) {
 		if (result == 0) {
 			setFlag(1, Z_POS);
 		} else {
 			setFlag(0, Z_POS);
 		}
-		setFlag(maskInt(result, 31, 31), N_POS);
+		setFlag(maskBits(result, 31, 31), N_POS);
 	}
 }
 
@@ -287,27 +279,29 @@ uint32_t rotateRight(uint32_t number, uint32_t offset) {
 // if bit I is cleared, the operand2 is specified by a register rm. It is either
 // rotated by a constant value(if bit4 is cleared) or rotated by a least
 // significant byte of rs
-uint32_t dproc_getOperand2(uint32_t instruction) {
-    int iBit = maskInt(instruction, I_POS, I_POS);
+uint32_t dproc_getOperand2(state_t *state) {
+    int iBit = maskBits(state->fetched, I_POS, I_POS);
     uint32_t operand2;
     if (iBit) {
-        operand2 = maskInt(instruction, IMM_OPERAND_HIGH, IMM_OPERAND_LOW);
-        uint8_t rotate = maskInt(instruction, IMM_ROTATE_HIGH, IMM_ROTATE_LOW);
+        operand2 = maskBits(state->fetched, IMM_OPERAND_HIGH, IMM_OPERAND_LOW);
+        uint8_t rotate = maskBits(state->fetched, IMM_ROTATE_HIGH, 
+                IMM_ROTATE_LOW);
         operand2 = rotateRight(operand2, rotate * 2);
         return operand2;
     }
-    uint8_t rm = maskInt(instruction, REG_RM_HIGH, REG_RM_LOW);
+    uint8_t rm = maskBits(state->fetched, REG_RM_HIGH, REG_RM_LOW);
 
-    uint8_t bit4 = maskInt(instruction, 4, 4);
-    uint8_t shiftType = maskInt(instruction, SHIFT_TYPE_LOW, SHIFT_TYPE_HIGH);
+    uint8_t bit4 = maskBits(state->fetched, 4, 4);
+    uint8_t shiftType = maskBits(state->fetched, SHIFT_TYPE_LOW, 
+            SHIFT_TYPE_HIGH);
     uint32_t offset;
     if (bit4) {
-        uint8_t rs = maskInt(instruction, REG_RS_HIGH, REG_RS_LOW);
-        offset = maskInt(state.registers[rs], 7, 0);
+        uint8_t rs = maskInt(state->fetched, REG_RS_HIGH, REG_RS_LOW);
+        offset = maskInt(state->registers[rs], 7, 0);
     } else {
-        offset = maskInt(instruction, SHIFT_CONST_HIGH, SHIFT_CONST_LOW);
+        offset = maskInt(state->fetched, SHIFT_CONST_HIGH, SHIFT_CONST_LOW);
     }
-    operand2 = shift(shiftType, state.registers[rm], offset);
+    operand2 = shift(shiftType, state->registers[rm], offset);
     return operand2;
 }
 
@@ -349,96 +343,97 @@ uint32_t shift(uint8_t type, uint32_t number, uint32_t offset) {
 }
 
 // AND rd = operand2 AND rn
-uint32_t dproc_and(uint32_t operand2,
+uint32_t dproc_and(state_t *state, uint32_t operand2,
 		uint8_t rn, uint8_t rd) {
 
-	uint32_t result = state.registers[rn] & operand2;
-	state.registers[rd] = result;
+	uint32_t result = state->registers[rn] & operand2;
+	state->registers[rd] = result;
 	return result;
 }
 
 
 // XOR rd = operand2 XOR rn
-uint32_t dproc_eor(uint32_t operand2, uint8_t rn, uint8_t rd) {
-    uint32_t result = state.registers[rn] ^ operand2;
-    state.registers[rd] = result;
+uint32_t dproc_eor(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd) {
+    uint32_t result = state->registers[rn] ^ operand2;
+    state->registers[rd] = result;
     return result;
 }
 
 // Substraction rd = operand2 - rn, if rn > operand2, carry generated
-uint32_t dproc_sub(uint32_t operand2, uint8_t rn, uint8_t rd) {
+uint32_t dproc_sub(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd) {
     if ((int32_t) state.registers[rn] < (int32_t)operand2) {
         setFlag(1, C_POS);
     } else {
         setFlag(0, C_POS);
     }
-	uint32_t result = (int32_t) state.registers[rn] - (int32_t)operand2;
-	state.registers[rd] = result;
+	uint32_t result = (int32_t) state->registers[rn] - (int32_t)operand2;
+	state->registers[rd] = result;
 	return result;
 }
 
 // Substraction rd = operand2 - rn, if rn > operand2, carry generated
-uint32_t dproc_rsb(uint32_t operand2, uint8_t rn, uint8_t rd) {
-    if ((int32_t)operand2 < (int32_t) state.registers[rn]) {
+uint32_t dproc_rsb(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd) {
+    if ((int32_t)operand2 < (int32_t) state->registers[rn]) {
         setFlag(1, C_POS);
     } else {
         setFlag(0, C_POS);
     }
-	uint32_t result = operand2 - (int32_t) state.registers[rn];
-	state.registers[rd] = result;
+	uint32_t result = operand2 - (int32_t) state->registers[rn];
+	state->registers[rd] = result;
 	return result;
 }
 
 // Additiotion rd = rn + operand2, unsigned overflow sets carry flag
-uint32_t dproc_add(uint32_t operand2, uint8_t rn, uint8_t rd) {
-    if ((int32_t) state.registers[rn]  > INT_MAX - (int32_t)operand2) {
+uint32_t dproc_add(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd) {
+    if ((int32_t) state->registers[rn]  > INT_MAX - (int32_t)operand2) {
         setFlag(1, C_POS);
     } else {
         setFlag(0, C_POS);
     }
-	uint32_t result = (int32_t) state.registers[rn] + (int32_t)operand2;
+	uint32_t result = (int32_t) state->registers[rn] + (int32_t)operand2;
 	state.registers[rd] = result;
 	return result;
 }
 
 // Test AND on operand2 and rn, result not saved
-uint32_t dproc_tst(uint32_t operand2, uint8_t rn) {
-	uint32_t result = state.registers[rn] & operand2;
+uint32_t dproc_tst(state_t *state, uint32_t operand2, uint8_t rn) {
+	uint32_t result = state->registers[rn] & operand2;
 	return result;
 }
 
 
 // Test XOR on operand2 and rn, result not saved
-uint32_t dproc_teq(uint32_t operand2, uint8_t rn) {
-	uint32_t result = state.registers[rn] ^ operand2;
+uint32_t dproc_teq(state_t *state, uint32_t operand2, uint8_t rn) {
+	uint32_t result = state->registers[rn] ^ operand2;
 	return result;
 }
 
 // Compare rn and operand2, if operand2 if bigger, carry flag is set
-uint32_t dproc_cmp(uint32_t operand2, uint8_t rn) {
-    if ((int32_t) state.registers[rn] < (int32_t)operand2) {
+uint32_t dproc_cmp(state_t *state, uint32_t operand2, uint8_t rn) {
+    if ((int32_t) state->registers[rn] < (int32_t)operand2) {
         setFlag(1, C_POS);
     } else {
         setFlag(0, C_POS);
     }
-	uint32_t result = (int32_t) state.registers[rn] - (int32_t)operand2;
+	uint32_t result = (int32_t) state->registers[rn] - (int32_t)operand2;
 	return result;
 }
 
 // Bitwise or on operand2 and rn, result saved in rd
-uint32_t dproc_orr(uint32_t operand2, uint8_t rn, uint8_t rd) {
+uint32_t dproc_orr(state_t *state, uint32_t operand2, uint8_t rn, uint8_t rd) {
 	uint32_t result = state.registers[rn] | operand2;
-	state.registers[rd] = result;
+	state->registers[rd] = result;
 	return result;
 }
 
 // Moves the operand2 to register rd
-uint32_t dproc_mov(uint32_t operand2, uint8_t rd) {
+uint32_t dproc_mov(state_t *state, uint32_t operand2, uint8_t rd) {
 	uint32_t result = operand2;
-	state.registers[rd] = result;
+	state->registers[rd] = result;
 	return result;
 }
 
+/*
 void multiply(uint32_t instruction) {
     uint32_t result;
     if (checkCond(instruction) == 1) {
@@ -501,7 +496,7 @@ void branch(uint32_t instruction) {
     }
 }
 
-
+*/
 state_t *newState(void) {
     state_t *state = malloc(sizeof(state_t));
     if (state == NULL) {
