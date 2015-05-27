a0788209129097826fac31593279642a610bc048
Yifan Wu <yw8012@ic.ac.uk>
2015-05-27 15:08:16 +0100

    Basic linked map symbol table

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,40 @@
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef struct linked_map {
+    char *string;
+    uint32_t integer;
+    struct linked_map *next;
+} map_t;
+
+void put(map_t *root, char *string, uint32_t integer);
+uint32_t get(map_t *root, char *string);
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
+}
+
+void put(map_t *root, char *string, uint32_t integer) {
+    if (root == NULL) {
+        root = malloc(sizeof(map_t));
+        root->string = string;
+        root->integer = integer;
+        root->next = NULL;
+    }
+    while (root->next != NULL) {
+        root = root->next;
+    }
+    map_t *newMap = malloc(sizeof(map_t));
+    newMap->string = string;
+    newMap->integer = integer;
+    newMap->next = NULL;
+    root->next = newMap;
+}
+
+uint32_t get(map_t *root, char *string) {
+    while (strcmp(root->string, string)) {
+        root = root->next;
+    }
+    return root->integer;
 }
