43d0e54191ac1cfb65522b5d3fd2f36d739a2756
Paul Balaji <pmb14@ic.ac.uk>
2015-06-03 17:41:54 +0100

    did some updates to gpio.s and created a bash script to assemble and run

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,21 +1,52 @@
 ldr r0,=0x20200000 ;base address
-mov r1,#1
+mov r1,#1 ;output 17
 lsl r1,#21
 str r1,[r0,#4] 
 
+mov r1,#1 ;output 18
+lsl r1,#24
+str r1,[r0,#4] 
+
+mov r1,#1 ;output 4
+lsl r1,#12
+str r1,[r0,#0] 
+
+mov r3, #0
 loop:
-mov r2,#0x0F0000
 str r1,[r0,#40]
 
+tst r3, #1
+beq on2
+mov r1,#1 ;output 17
+lsl r1,#21
+str r1,[r0,#4] 
+
+on2:
+tst r3, #2
+beq on3
+mov r1,#1 ;output 18
+lsl r1,#24
+str r1,[r0,#4] 
+
+on3:
+tst r3, #4
+beq wait
+mov r1,#1 ;output 4
+lsl r1,#12
+str r1,[r0,#0] 
+
+mov r2,#0x0F0000
 wait:
 sub r2,r2,#1
 cmp r2,#0xFF
 bne wait
-str r1,[r0,#28]
-mov r2,#0x0F0000
 
-wait2:
-sub r2,r2,#1
-cmp r2,#0xFF
-bne wait2
+cmp r3, #8
+bne increment
+mov r3, #0
+
+increment:
+add r3, r3, #1
+
+str r1,[r0,#28]
 b loop
