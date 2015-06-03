c75e6e6d434e03de4980f6f3650f09b5a994202c
Paul Balaji <pmb14@ic.ac.uk>
2015-06-03 17:53:40 +0100

    switching to Jan's computer

--- programs/gpio.s
+++ programs/gpio.s
@@ -9,21 +9,22 @@ str r1,[r0,#4]
 
 mov r1,#1 ;output 4
 lsl r1,#12
-str r1,[r0,#0] 
+str r1,[r0] 
 
-mov r3, #0
+mov r3,#0
 loop:
+mov r1,
 str r1,[r0,#40]
 
 tst r3, #1
-beq on2
+bne on2
 mov r1,#1 ;output 17
 lsl r1,#21
 str r1,[r0,#4] 
 
 on2:
 tst r3, #2
-beq on3
+bne on3
 mov r1,#1 ;output 18
 lsl r1,#24
 str r1,[r0,#4] 
