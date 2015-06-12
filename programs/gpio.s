ed40fe2b0b570b309d3c55119d5533d6cb78e02b
Yifan Wu <yw8012@ic.ac.uk>
2015-06-12 15:19:24 +0100

    Update gpio.s

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,21 +1,26 @@
 ldr r0,=0x20200000 ;base address
 mov r1,#1
-lsl r1,#21
-str r1,[r0,#4] 
+lsl r1,#18
+str r1,[r0,#4] ;set GPIO 16 as output pin
+
+mov r1,r1,lsr #2 ;set bit 16
 
 loop:
-mov r2,#0x0F0000
-str r1,[r0,#40]
 
-wait:
+str r1,[r0,#40] ;clear GPIO 16
+
+mov r2,#0xF0000
+wait1:
 sub r2,r2,#1
-cmp r2,#0xFF
-bne wait
-str r1,[r0,#28]
-mov r2,#0x0F0000
+cmp r2,#0
+bne wait1
 
+str r1,[r0,#28] ;set GPIO 16
+
+mov r2,#0xF0000
 wait2:
 sub r2,r2,#1
-cmp r2,#0xFF
+cmp r2,#0
 bne wait2
+
 b loop
