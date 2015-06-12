68321e8ac1b97422cc015346b809199599fe421e
Yifan Wu <yw8012@ic.ac.uk>
2015-06-12 15:43:00 +0100

    Update to 5 bits binary counter

--- programs/binCounter.s
+++ programs/binCounter.s
@@ -1,53 +1,57 @@
 ldr r0,=0x20200000 ;base address
-mov r1,#1 ;output 17
-lsl r1,#21
-str r1,[r0,#4] 
+mov r1,#1
 
-mov r1,#1 ;output 18
-lsl r1,#24
-str r1,[r0,#4] 
+mov r2,r1,lsl #12
+str r1,[r0] ;set GPIO 4 as output pin
 
-mov r1,#1 ;output 4
-lsl r1,#12
-str r1,[r0] 
+mov r2,r1,lsl #21
+str r1,[r0,#4] ;set GPIO 17 as output pin
 
-mov r3,#0
+mov r2,r1,lsl #24
+str r1,[r0,#4] ;set GPIO 18 as output pin
+
+mov r2,r1,lsl #9
+str r1,[r0,#8] ;set GPIO 23 as output pin
+
+mov r2,r1,lsl #12
+str r1,[r0,#8] ;set GPIO 24 as output pin
+
+mov r3,#0 ;the count
+
 loop:
-mov r1,
-str r1,[r0,#40]
-
-tst r3, #1
-bne on2
-mov r1,#1 ;output 17
-lsl r1,#21
-str r1,[r0,#4] 
-
-on2:
-tst r3, #2
-bne on3
-mov r1,#1 ;output 18
-lsl r1,#24
-str r1,[r0,#4] 
-
-on3:
-tst r3, #4
-beq wait
-mov r1,#1 ;output 4
-lsl r1,#12
-str r1,[r0,#0] 
-
-mov r2,#0x0F0000
+mov r4,#0xF00000
 wait:
-sub r2,r2,#1
-cmp r2,#0xFF
+sub r4,r4,#1
+cmp r4,#0
 bne wait
 
-cmp r3, #8
-bne increment
-mov r3, #0
+mov r2,r1,lsl #4
+tst r3,r1 ;test whether bit 0 is 0 or 1
+streq r2,[r0,#40] ;clear GPIO 4 if bit 0 is 0
+strne r2,[r0,#28] ;set GPIO 4 if bit 0 is 1
 
-increment:
-add r3, r3, #1
+mov r2,r1,lsl #17 ;test bit 1 and change GPIO 17
+tst r3,r1,lsl #1
+streq r2,[r0,#40]
+strne r2,[r0,#28]
 
-str r1,[r0,#28]
+mov r2,r1,lsl #18 ;test bit 2 and change GPIO 18
+tst r3,r1,lsl #2
+streq r2,[r0,#40]
+strne r2,[r0,#28]
+
+mov r2,r1,lsl #23 ;test bit 3 and change GPIO 23
+tst r3,r1,lsl #3
+streq r2,[r0,#40]
+strne r2,[r0,#28]
+
+mov r2,r1,lsl #24 ;test bit 4 and change GPIO 24
+tst r3,r1,lsl #4
+streq r2,[r0,#40]
+strne r2,[r0,#28]
+
+add r3,r3,#1
+cmp r3,#32 ;reset to 0 if counter overflowed
+moveq r3,#0
+
 b loop
